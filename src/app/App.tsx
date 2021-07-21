6a65152b26dd0bd7a789595f6681de05e9005221
richardadda <1156282805@qq.com>
2021-07-21 22:59:38 +0800

    refactor: modify - upadte react-dom dependencies

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Helmet } from 'react-helmet-async'
 import Root from './pages/Root'
 import favicon from './asserts/images/log.jpg'
@@ -13,10 +13,12 @@ const App = (props: any) => {
 	const __root_id__ = Math.random()
 	return (
 		<>
+			{/* <Suspense fallback={<section>Loading</section>}> */}
 			<Helmet link={[{ rel: 'icon', type: 'image/jpg', href: favicon }]}>
 				<title>React Application</title>
 			</Helmet>
 			<Root __RootProps__={{ __root_id__ }} {...props} />
+			{/* </Suspense> */}
 		</>
 	)
 }
