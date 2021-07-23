9712186d3c146f76b3e93b0138481c464e701444
ctime-zliang <1156282805@qq.com>
2021-07-23 18:07:28 +0800

    fix: modify - fixed file directory error

--- src/server/error.ts
+++ src/server/error.ts
@@ -1,4 +1,4 @@
-import { IExtendKoaContext } from '../types/koa-context'
+import { IExtendKoaContext } from './types/koa-context'
 
 const isErrorWithStatus = (error: any) => {
 	if (typeof error['status'] == 'number' && error['error'] && error['error']['stack']) {
