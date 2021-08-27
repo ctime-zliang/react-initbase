f5b63d1ca84542165da064787d852106d22a4a2b
ctime-zliang <1156282805@qq.com>
2021-08-27 18:06:41 +0800

    feat: modify - add hook: use.reactive

--- src/app/pages/TestModules/index.tsx
+++ src/app/pages/TestModules/index.tsx
@@ -1,24 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Parent } from './ReactForwardRef'
 
-let count = 0
 function TestsRoot(props: any) {
-	const [list, setList] = useState<any[]>([])
-	const clickAction = () => {
-		// count++
-		// const a: any[] = []
-		// for (let i = 0; i < 10000; i++) {
-		// 	a.push(count)
-		// }
-		// setList(a)
-	}
 	return (
 		<>
-			<div onClick={clickAction}>Test</div>
-			<ul>
-				{list.map((item: any, index: number) => {
-					return <li key={index}>{item}</li>
-				})}
-			</ul>
+			<Parent />
 		</>
 	)
 }
