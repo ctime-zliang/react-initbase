7b1650200b5de401d47954a2af6a4715a5fe8239
ctime-zliang <1156282805@qq.com>
2022-03-09 11:38:24 +0800

    feat: modify -daily commit

--- config/utils.js
+++ config/utils.js
@@ -8,16 +8,6 @@ module.exports = {
 	resolveDirectory(relativePath) {
 		return path.resolve(ApplicationDirectory, relativePath)
 	},
-	syncReadJsonFile(path, encode = 'utf-8') {
-		try {
-			const fileContent = fs.readFileSync(path, encode)
-			return JSON.parse(fileContent)
-		} catch (e) {
-			console.log(`Read JSON File Error`)
-			console.log(e)
-			return null
-		}
-	},
 	timeStamp() {
 		const d = new Date()
 		const arr = [d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds()]
@@ -38,23 +28,5 @@ module.exports = {
 	},
 	jestCoverage() {
 		return process.argv.includes('--coverage')
-	},
-	createLoaderResult(string, isEsm = false) {
-		const prefix = isEsm ? 'export default ' : 'module.exports = '
-		return prefix + string
-	},
-	getStringExportContent(exportString) {
-		try {
-			if (/module.exports(.*)/gi.test(exportString)) {
-				return exportString.replace(/module.exports(\s+)=(\s+)/gi, '')
-			}
-			if (/export default(.*)/gi.test(exportString)) {
-				return exportString.replace(/export default(\s+)/gi, '')
-			}
-			return exportString
-		} catch (e) {
-			console.log(e)
-			return
-		}
 	},
 }
