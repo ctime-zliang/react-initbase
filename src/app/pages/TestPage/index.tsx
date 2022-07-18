1dd418c0eb4e4a12b12861c788f43434b96ed78c
ctime-zliang <1156282805@qq.com>
2022-07-18 23:47:05 +0800

    feat: modify - test demo change

--- src/app/pages/TestPage/index.tsx
+++ src/app/pages/TestPage/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Parent as ReactForwardRefParent } from './ReactForwardRef'
 import { Wrapper as AsyncSetStateOfClassComponentWrapper } from './AsyncSetStateOfClassComponent'
 import { Wrapper as UseEffectExecSequentialWrapper } from './UseEffectExecSequential'
+import { AddLongChild } from './AddChild'
 
 function TestPageRoot(props: any) {
 	return (
 		<>
-			<UseEffectExecSequentialWrapper />
+			<AddLongChild />
 		</>
 	)
 }
